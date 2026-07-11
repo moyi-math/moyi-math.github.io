f4d9b64c7a2dc7cff82eaa0357cabac755d3483e
moyi-math <moyi@github.com>
2026-07-11 17:25:17 +0800

    Site updated: 2026-07-11 17:25:16

---
+++ js/canvas-nest-config.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var script = document.createElement("script");
+  script.src = "https://cdn.bootcss.com/canvas-nest.js/1.0.0/canvas-nest.min.js";
+  script.setAttribute("count", "150");
+  script.setAttribute("opacity", "1");
+  document.body.appendChild(script);
+});
